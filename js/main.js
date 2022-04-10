b1c0f134822f2a8e8866a73a668af3b794834966
Lazarenco Vera <88059178+Veronela@users.noreply.github.com>
2022-04-10 15:36:36 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function data(){
+    var span;
+    var date=new Date();
+    var an;
+
+    an= date.getFullYear();
+
+    span=document.getElementById("data");
+    span.innerHTML="Toate drepturile de autor sunt rezervate"
+}
